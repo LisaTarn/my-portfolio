cd1efd8a6793d6009ef3de0539326cfb960cd599
LisaTarn <lisatarnowpt@gmail.com>
2024-11-10 10:14:42 -0500

    Adding form structure and implemeting javascript validation

--- script.js
+++ script.js
@@ -65,3 +65,97 @@ function handleBtnClick1(event){
         blankParagraph.innerHTML = "      "
     }
   }
+
+  const form = document.querySelector("form");
+  const email = document.getElementById("email");
+  const emailError = document.querySelector("#email + span.error");
+  const fullname = document.getElementById("fullname");
+  const nameError = document.querySelector("#fullname + span.error")
+  const message = document.getElementById("message");
+  const messageError = document.querySelector("#message + span.error");
+
+  fullname.addEventListener("input", () => {
+    if (fullname.validity.valid) {
+      nameError.textContent ="";
+      nameError.className="error";
+    } else{
+      showErrorName();
+    }
+  })
+
+  email.addEventListener("input", () => {
+    if (email.validity.valid) {
+      emailError.textContent = ""; // Remove the message content
+      emailError.className = "error"; // Removes the `active` class
+    } else {
+      // If there is still an error, show the correct error
+      showErrorEmail();
+    }
+  });
+
+  message.AddEventListener("input", () => {
+    if (message.validity.valid) {
+      messageError.textContent ="";
+      messageError.className="error";
+    } else{
+      showErrorMessage();
+    }
+  })
+  
+  form.addEventListener("submit", (event) => {
+    // if the email field is invalid
+    if (!email.validity.valid) {
+      // display an appropriate error message
+      showErrorEmail();
+      // prevent form submission
+      event.preventDefault();
+    } 
+    else if (!fullname.validity.valid) {
+      showErrorName();
+      event.preventDefault();
+    }
+    else if (!message.validity.valid){
+      showErrorMessage ()
+      event.preventDefault();
+    }
+  });
+  
+  function showErrorName() {
+    if (fullname.validity.valueMissing) {
+      // If empty
+      nameError.textContent = "You need to enter a name.";
+    } else if (fullname.validity.typeMismatch) {
+      // If it's not a name
+      nameError.textContent = "Entered value needs to be a name.";
+    }
+    // Add the `active` class
+    nameError.className = "error active";
+  }
+  
+  function showErrorEmail() {
+    if (email.validity.valueMissing) {
+      // If empty
+      emailError.textContent = "You need to enter an email address.";
+    } else if (email.validity.typeMismatch) {
+      // If it's not an email address,
+      emailError.textContent = "Entered value needs to be an email address.";
+    } else if (email.validity.tooShort) {
+      // If the value is too short,
+      emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    }
+    // Add the `active` class
+    emailError.className = "error active";
+  }
+  
+  function showErrorMessage() {
+    if (message.validity.valueMissing) {
+      // If empty
+      messageError.textContent = "You need to enter a a message";
+    } else if (message.validity.typeMismatch) {
+      // If it's not a message,
+      messageError.textContent = "Entered value needs to be a message.";
+    } 
+    // Add the `active` class
+    messageError.className = "error active";
+  }
+  
