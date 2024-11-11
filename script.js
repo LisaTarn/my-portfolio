fd698c5e1e2c36ee32cab2aa624407be167a71e4
LisaTarn <lisatarnowpt@gmail.com>
2024-11-11 12:32:50 -0500

    Changed up toggle button code

--- script.js
+++ script.js
@@ -2,69 +2,44 @@ const project1Button = document.getElementById("project1-btn")
 const project2Button = document.getElementById("project2-btn")
 const project3Button = document.getElementById("project3-btn")
 
-project1Button.addEventListener ("click", handleBtnClick1 )
-project2Button.addEventListener ("click", handleBtnClick2 )
-project3Button.addEventListener ("click", handleBtnClick3 )
+project1Button.addEventListener ("click", toggleelement1() );
+project2Button.addEventListener ("click", toggleelement2() );
+project3Button.addEventListener ("click", toggleelement3() );
 
-function handleBtnClick1(event){
-    toggleButton1(event.target);
-}
 
-  function toggleButton1(element){
-    const pressed = element.getAttribute("aria-pressed") === "true";
-    element.setAttribute("aria-pressed", !pressed);
-    const newParagraph = document.createElement ('p');
-    newParagraph.textContent = "Project 1 Details";
-    if (pressed) {
-    const newParagraph = document.createElement ('p');
-    newParagraph.textContent = "Project 1 Details are here";
-    document.getElementById('project1').appendChild(newParagraph);
-    }
-    else{
-        const blankParagraph = document.getElementById('project1');
-        blankParagraph.innerHTML = "      "
-    }
+function toggleelement1(){
+  const paragraph= document.getElementById('project1');
+  const currentVisibility = window.getComputedStyle(paragraph).visibility;
+
+  if (currentVisibility === 'hidden'){
+    paragraph.style.visibility = 'visible';
+  }else{
+    paragraph.style.visibility = 'hidden';
+  }
   }
 
-  function handleBtnClick2(event){
-    toggleButton2(event.target);
-}
+  function toggleelement2(){
+    const paragraph= document.getElementById('project2');
+    const currentVisibility = window.getComputedStyle(paragraph).visibility;
+  
+    if (currentVisibility === 'hidden'){
+      paragraph.style.visibility = 'visible';
+    }else{
+      paragraph.style.visibility = 'hidden';
+    }
+    }
 
-  function toggleButton2(element){
-    const pressed = element.getAttribute("aria-pressed") === "true";
-    element.setAttribute("aria-pressed", !pressed);
-    const newParagraph = document.createElement ('p');
-    newParagraph.textContent = "Project 2 Details";
-    if (pressed) {
-    const newParagraph = document.createElement ('p');
-    newParagraph.textContent = "Project 2 Details are here";
-    document.getElementById('project2').appendChild(newParagraph);
-    }
-    else{
-        const blankParagraph = document.getElementById('project2');
-        blankParagraph.innerHTML = "      "
-    }
-  }
-
-  function handleBtnClick3(event){
-    toggleButton3(event.target);
-}
-
-  function toggleButton3(element){
-    const pressed = element.getAttribute("aria-pressed") === "true";
-    element.setAttribute("aria-pressed", !pressed);
-    const newParagraph = document.createElement ('p');
-    newParagraph.textContent = "Project 3 Details";
-    if (pressed) {
-    const newParagraph = document.createElement ('p');
-    newParagraph.textContent = "Project 3 Details are here";
-    document.getElementById('project3').appendChild(newParagraph);
-    }
-    else{
-        const blankParagraph = document.getElementById('project3');
-        blankParagraph.innerHTML = "      "
-    }
-  }
+    function toggleelement3(){
+      const paragraph= document.getElementById('project3');
+      const currentVisibility = window.getComputedStyle(paragraph).visibility;
+    
+      if (currentVisibility === 'hidden'){
+        paragraph.style.visibility = 'visible';
+      }else{
+        paragraph.style.visibility = 'hidden';
+      }
+      }
+  
 
   const form = document.querySelector("form");
   const email = document.getElementById("email");
@@ -159,3 +134,4 @@ function handleBtnClick1(event){
     messageError.className = "error active";
   }
   
+
